c1e312f9e9730c05e2aa48e9428d08dd681df994
awrde <41605569+awrde@users.noreply.github.com>
2019-01-30 02:52:06 +0900

    Add files via upload

---
+++ python 반복문 1회차 (실패).py
@@ -0,0 +1,1047 @@
+Python 3.7.1 (default, Dec 10 2018, 22:09:34) [MSC v.1915 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num=0
+>>> while num<10:
+	if num 2 == 1:
+		
+SyntaxError: invalid syntax
+>>> while num<10:
+	if num % 2==1:
+		prnt(num)
+	num = num+1
+
+	
+Traceback (most recent call last):
+  File "<pyshell#7>", line 3, in <module>
+    prnt(num)
+NameError: name 'prnt' is not defined
+>>> while num<10:
+	if num % 2==1:
+		prnt(num)
+	num += 1
+
+	
+Traceback (most recent call last):
+  File "<pyshell#9>", line 3, in <module>
+    prnt(num)
+NameError: name 'prnt' is not defined
+>>> while num<10:
+	if num % 2==1:
+		print(num)
+	num = num+1
+
+	
+1
+3
+5
+7
+9
+>>> while 2:
+	print("find stocks")
+
+	
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+Traceback (most recent call last):
+  File "<pyshell#14>", line 2, in <module>
+    print("find stocks")
+KeyboardInterrupt
+>>> while 0.1:
+	print("find stocks")
+
+	
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+find stocks
+Traceback (most recent call last):
+  File "<pyshell#17>", line 2, in <module>
+    print("find stocks")
+KeyboardInterrupt
+>>> num=0
+>>> while 1:
+	num += 1
+	if num == 10:
+		break
+	print(num)
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> #1
+>>> 
+>>> 1
+1
+>>> #주석입니다.
+>>> 
+>>> num
+10
+>>> num=10
+>>> num=0
+>>> while 1:
+	print(num)
+	num += 1
+	if num ==5:
+		continue
+	if num >9:
+		break
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> num=0
+>>> while <10:
+	
+SyntaxError: invalid syntax
+>>> while num<10:
+	num+=1
+	if num== 5:
+		continue
+	print(num)
+
+	
+1
+2
+3
+4
+6
+7
+8
+9
+10
+>>> while num<10:
+	num+=1
+	if num==5:
+		pass
+	print(num)
+
+	
+>>> 
+>>> num=0
+>>> while num<10:
+	num+=1
+	if num==5:
+		pass
+	print(num)
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+>>> pass
+>>> print('')
+
+>>> for i in [1, 2,3,4]:
+	for j in [1,2,3,4]:
+		pass
+
+	
+>>> i
+4
+>>> j
+4
+>>> typei
+Traceback (most recent call last):
+  File "<pyshell#75>", line 1, in <module>
+    typei
+NameError: name 'typei' is not defined
+>>> type(i)
+<class 'int'>
+>>> for i in [1, 2,3,4]:
+	for j in [1,2,3,4]:
+		print(i,j)
+
+		
+1 1
+1 2
+1 3
+1 4
+2 1
+2 2
+2 3
+2 4
+3 1
+3 2
+3 3
+3 4
+4 1
+4 2
+4 3
+4 4
+>>> for i in [1,2,3,4]:
+	for j in[1,2,3,4]:
+		print("newspaper delivery: ", i,0,j)
+
+		
+newspaper delivery:  1 0 1
+newspaper delivery:  1 0 2
+newspaper delivery:  1 0 3
+newspaper delivery:  1 0 4
+newspaper delivery:  2 0 1
+newspaper delivery:  2 0 2
+newspaper delivery:  2 0 3
+newspaper delivery:  2 0 4
+newspaper delivery:  3 0 1
+newspaper delivery:  3 0 2
+newspaper delivery:  3 0 3
+newspaper delivery:  3 0 4
+newspaper delivery:  4 0 1
+newspaper delivery:  4 0 2
+newspaper delivery:  4 0 3
+newspaper delivery:  4 0 4
+>>> for i in [1,2,3,4]:
+	for j in[1,2,3,4]:
+		print("newspaper delivery: ", j0i)
+
+		
+Traceback (most recent call last):
+  File "<pyshell#84>", line 3, in <module>
+    print("newspaper delivery: ", j0i)
+NameError: name 'j0i' is not defined
+>>> for i in [1,2,3,4]:
+	for j in[1,2,3,4]:
+		print("newspaper delivery: ", j,0,i)
+
+		
+newspaper delivery:  1 0 1
+newspaper delivery:  2 0 1
+newspaper delivery:  3 0 1
+newspaper delivery:  4 0 1
+newspaper delivery:  1 0 2
+newspaper delivery:  2 0 2
+newspaper delivery:  3 0 2
+newspaper delivery:  4 0 2
+newspaper delivery:  1 0 3
+newspaper delivery:  2 0 3
+newspaper delivery:  3 0 3
+newspaper delivery:  4 0 3
+newspaper delivery:  1 0 4
+newspaper delivery:  2 0 4
+newspaper delivery:  3 0 4
+newspaper delivery:  4 0 4
+>>> apart = [[i,0,j]]
+>>> apart
+[[4, 0, 4]]
+>>> apart = [[101,102,103,104],[201,202,203,204],[301,302,303,304],[401,402,403,404]]
+>>> apart
+[[101, 102, 103, 104], [201, 202, 203, 204], [301, 302, 303, 304], [401, 402, 403, 404]]
+>>> apart[0]
+[101, 102, 103, 104]
+>>> type apart
+SyntaxError: invalid syntax
+>>> type(apart)
+<class 'list'>
+>>> type( apart )
+<class 'list'>
+>>> type(apart[0])
+<class 'list'>
+>>> type(apart[0][0])
+<class 'int'>
+>>> for i in [1,2,3,4]:
+	for j in[1,2,3,4]:
+		print("newspaper delivery: ", j,0,i)
+
+		
+newspaper delivery:  1 0 1
+newspaper delivery:  2 0 1
+newspaper delivery:  3 0 1
+newspaper delivery:  4 0 1
+newspaper delivery:  1 0 2
+newspaper delivery:  2 0 2
+newspaper delivery:  3 0 2
+newspaper delivery:  4 0 2
+newspaper delivery:  1 0 3
+newspaper delivery:  2 0 3
+newspaper delivery:  3 0 3
+newspaper delivery:  4 0 3
+newspaper delivery:  1 0 4
+newspaper delivery:  2 0 4
+newspaper delivery:  3 0 4
+newspaper delivery:  4 0 4
+>>> # 4-1 문제 4-1
+
+아래와 같은 패턴의 별(*)을 출력하는 프로그램을 작성해 보세요. 참고로 print('*', end='')와 같이 print 함수를 사용하면 줄바꿈 없이 화면에 출력할 수 있습니다.
+SyntaxError: invalid syntax
+>>> # 4-1 문제 4-1
+
+#아래와 같은 패턴의 별(*)을 출력하는 프로그램을 작성해 보세요. 참고로 print('*', end='')와 같이 print 함수를 사용하면 줄바꿈 없이 화면에 출력할 수 있습니다.
+>>> print(*****)
+SyntaxError: invalid syntax
+>>> for i in [*,*,*,*,*}
+SyntaxError: invalid syntax
+>>> 
+>>> print("*****")
+*****
+>>> #문제 4-2
+
+#아래와 같은 패턴의 별(*)을 출력하는 프로그램을 작성해보세요. (힌트: 중첩 루프 사용)
+
+#*****
+#*****
+#*****
+#*****
+>>> n=1
+    while n>4:
+	print("*****")
+	n+=1
+	
+SyntaxError: multiple statements found while compiling a single statement
+>>> n=1
+>>> while n>4:
+	print("*****")
+	n+=1
+
+	
+>>> n=
+SyntaxError: invalid syntax
+>>> 
+>>> n
+1
+>>> while n>4:
+	n+=1
+
+	
+>>> 
+>>> print("*****")*4
+*****
+Traceback (most recent call last):
+  File "<pyshell#123>", line 1, in <module>
+    print("*****")*4
+TypeError: unsupported operand type(s) for *: 'NoneType' and 'int'
+>>> print("*****")/n
+*****
+Traceback (most recent call last):
+  File "<pyshell#124>", line 1, in <module>
+    print("*****")/n
+TypeError: unsupported operand type(s) for /: 'NoneType' and 'int'
+>>> a=*****
+SyntaxError: invalid syntax
+>>> a="*****"
+>>> a
+'*****'
+>>> print(a*5)
+*************************
+>>> for a in(1,2,3,4,5) print(a)
+SyntaxError: invalid syntax
+>>> for a in(1,2,3,4,5): print(a)
+
+1
+2
+3
+4
+5
+>>> a
+5
+>>> a="*"
+>>> for a in("*","**","***","****","*****"):
+	print(a)
+
+	
+*
+**
+***
+****
+*****
+>>> for a in("    *","   **","  ***"," ****","*****"):
+	print(a)
+
+    *
+   **
+  ***
+ ****
+*****
+>>> for a in("a*2"):
+	a="*"
+	print(a)
+
+	
+*
+*
+*
+>>> for a in(1,2,3,4):
+	for b in(1,2,3,4):
+		print(a,0,b)
+		if a==b and a=1
+		
+SyntaxError: invalid syntax
+>>> for a in(1,2,3,4):
+	for b in(1,2,3,4):
+		print(a,0,b)
+		if a==b and a==1
+		
+SyntaxError: invalid syntax
+>>> for a in(1,2,3,4):
+	for b in(1,2,3,4):
+		print(a,0,b)
+		if a==b and a==1 continue
+		
+SyntaxError: invalid syntax
+>>> for a in(1,2,3,4):
+	for b in(1,2,3,4):
+		print(a,0,b)
+
+	
+1 0 1
+1 0 2
+1 0 3
+1 0 4
+2 0 1
+2 0 2
+2 0 3
+2 0 4
+3 0 1
+3 0 2
+3 0 3
+3 0 4
+4 0 1
+4 0 2
+4 0 3
+4 0 4
+>>> for a in(1,2,3,4):
+	for b in(1,2,3,4):
+		print(a,0,b)
+
+		
+1 0 1
+1 0 2
+1 0 3
+1 0 4
+2 0 1
+2 0 2
+2 0 3
+2 0 4
+3 0 1
+3 0 2
+3 0 3
+3 0 4
+4 0 1
+4 0 2
+4 0 3
+4 0 4
+>>> for i in range(5):
+	print("*", end='')
+
+	
+*****
+>>> range(5)_
+SyntaxError: invalid syntax
+>>> range(5)
+range(0, 5)
+>>> for i in range(4)
+SyntaxError: invalid syntax
+>>> for i in range(4):
+	for j in range(5):
+		print("*", end="")
+		print("")
+
+		
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+>>> for i in range(4):
+	for j in range(5):
+		print("*", end='')
+		print("")
+
+		
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+>>> for j in range(4):
+        for i in range(5):
+                print('*', end='')
+        print("")
+
+        
+*****
+*****
+*****
+*****
+>>> for a in range(4):
+        for b in range(5):
+                print('*', end='')
+        print("")
+
+        
+*****
+*****
+*****
+*****
+>>> for i in range(4):
+	for j in range(5):
+		print("*", end='')
+	print("")
+
+*****
+*****
+*****
+*****
+>>> 
